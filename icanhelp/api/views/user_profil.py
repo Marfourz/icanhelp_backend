c5d82404b2aacbe6a764e3790c616c00b5ab7d61
Marfourz BOUKARI <marfourzboukari@Mac-001.lan>
2025-09-10 14:27:24 +0200

    Gestion des fichiers statiques

--- icanhelp/api/views/user_profil.py
+++ icanhelp/api/views/user_profil.py
@@ -6,7 +6,6 @@ from api.mixins import UserProfilMixin
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from model.matching import predict_match
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -28,61 +27,61 @@ class UserProfilViewSet(UserProfilMixin, viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     
-    @action(detail=False, methods=['get'], url_path='search')
-    def search(self,request):
-        userProfil = self.get_user_profil()
+    # @action(detail=False, methods=['get'], url_path='search')
+    # def search(self,request):
+    #     userProfil = self.get_user_profil()
 
-        if request.GET.get('category_id') :
-            category = Category.objects.get(id=request.GET.get('category_id'))
-            categories = [category]
-            profils = UserProfil.objects.filter(
-                competences__category__in=categories
-            ).exclude(id=userProfil.id).distinct()
-        else:
-            # 🔹 Étape 1 : récupère les catégories préférées
-            categories_preferees = userProfil.get_categories()
+    #     if request.GET.get('category_id') :
+    #         category = Category.objects.get(id=request.GET.get('category_id'))
+    #         categories = [category]
+    #         profils = UserProfil.objects.filter(
+    #             competences__category__in=categories
+    #         ).exclude(id=userProfil.id).distinct()
+    #     else:
+    #         # 🔹 Étape 1 : récupère les catégories préférées
+    #         categories_preferees = userProfil.get_categories()
 
-            # 🔸 Étape 2 : profils avec catégories préférées
-            profils_preferes = UserProfil.objects.filter(
-                competences__category__in=categories_preferees
-            ).exclude(id=userProfil.id).distinct()
+    #         # 🔸 Étape 2 : profils avec catégories préférées
+    #         profils_preferes = UserProfil.objects.filter(
+    #             competences__category__in=categories_preferees
+    #         ).exclude(id=userProfil.id).distinct()
 
-            # 🔸 Étape 3 : profils avec les autres catégories
-            autres_profils = UserProfil.objects.exclude(
-                id__in=profils_preferes.values_list('id', flat=True)
-            ).exclude(id=userProfil.id).distinct()
+    #         # 🔸 Étape 3 : profils avec les autres catégories
+    #         autres_profils = UserProfil.objects.exclude(
+    #             id__in=profils_preferes.values_list('id', flat=True)
+    #         ).exclude(id=userProfil.id).distinct()
 
-            # 🔹 Étape 4 : fusion avec priorité
-            profils = list(profils_preferes) + list(autres_profils)
+    #         # 🔹 Étape 4 : fusion avec priorité
+    #         profils = list(profils_preferes) + list(autres_profils)
 
     
-        search_text = request.GET.get('search')
+    #     search_text = request.GET.get('search')
 
-        search_profils = []
-        if search_text:
-            scored_profils = []
-            for profil in profils:
-                max_score = 0
-                for competence in profil.competences.all():
-                    score = predict_match(search_text, competence.title)
-                    if score > max_score:
-                        max_score = score
-                if max_score >= LIMIT_TO_MATCH:
-                    scored_profils.append((profil, max_score))
+    #     search_profils = []
+    #     if search_text:
+    #         scored_profils = []
+    #         for profil in profils:
+    #             max_score = 0
+    #             for competence in profil.competences.all():
+    #                 score = predict_match(search_text, competence.title)
+    #                 if score > max_score:
+    #                     max_score = score
+    #             if max_score >= LIMIT_TO_MATCH:
+    #                 scored_profils.append((profil, max_score))
 
-            # 🔥 Trier par score décroissant
-            scored_profils.sort(key=lambda x: x[1], reverse=True)
+    #         # 🔥 Trier par score décroissant
+    #         scored_profils.sort(key=lambda x: x[1], reverse=True)
 
-            # Extraire uniquement les profils
-            filtered_profils = [profil for profil, score in scored_profils]
-        else:
-            filtered_profils = profils
+    #         # Extraire uniquement les profils
+    #         filtered_profils = [profil for profil, score in scored_profils]
+    #     else:
+    #         filtered_profils = profils
 
-        page = self.paginate_queryset(filtered_profils)
+    #     page = self.paginate_queryset(filtered_profils)
 
-        serializer = self.get_serializer(page, many=True)
+    #     serializer = self.get_serializer(page, many=True)
 
-        return self.get_paginated_response(serializer.data)
+    #     return self.get_paginated_response(serializer.data)
 
     
     @action(detail=False, methods=['get'], url_path='my_profil')
@@ -123,42 +122,42 @@ class UserProfilViewSet(UserProfilMixin, viewsets.ModelViewSet):
     #     return Response(serializer.data)
     
 
-    @action(detail=False, methods=['get'], url_path='search')
-    def search2(self, request):
-        query = request.GET.get('search')
-        if not query:
-            return Response({"error": "Search text is required"}, status=400)
+    # @action(detail=False, methods=['get'], url_path='search')
+    # def search2(self, request):
+    #     query = request.GET.get('search')
+    #     if not query:
+    #         return Response({"error": "Search text is required"}, status=400)
 
-        query_embedding = model.encode(query, convert_to_tensor=True)
+    #     query_embedding = model.encode(query, convert_to_tensor=True)
 
-        results = []
+    #     results = []
 
-        for user in UserProfil.objects.prefetch_related('competences_persornal').all():
-            comp_texts = [comp.nom for comp in user.competences_persornal.all()]
-            if not comp_texts:
-                continue
+    #     for user in UserProfil.objects.prefetch_related('competences_persornal').all():
+    #         comp_texts = [comp.nom for comp in user.competences_persornal.all()]
+    #         if not comp_texts:
+    #             continue
 
-            comp_embeddings = model.encode(comp_texts, convert_to_tensor=True)
-            similarities = util.cos_sim(query_embedding, comp_embeddings)[0]
+    #         comp_embeddings = model.encode(comp_texts, convert_to_tensor=True)
+    #         similarities = util.cos_sim(query_embedding, comp_embeddings)[0]
 
-            # Similarity moyenne comme score global
-            avg_score = float(torch.mean(similarities))
+    #         # Similarity moyenne comme score global
+    #         avg_score = float(torch.mean(similarities))
 
-            # Compétences matchées au-dessus d’un seuil
-            matched = [
-                {"competence": comp, "score": float(sim)}
-                for comp, sim in zip(comp_texts, similarities)
-                if sim > 0.5  # seuil à ajuster
-            ]
+    #         # Compétences matchées au-dessus d’un seuil
+    #         matched = [
+    #             {"competence": comp, "score": float(sim)}
+    #             for comp, sim in zip(comp_texts, similarities)
+    #             if sim > 0.5  # seuil à ajuster
+    #         ]
 
-            results.append({
-                "user": user.id,
-                "match_score": round(avg_score, 3),
-                "matched_competences": matched
-            })
+    #         results.append({
+    #             "user": user.id,
+    #             "match_score": round(avg_score, 3),
+    #             "matched_competences": matched
+    #         })
 
-        results.sort(key=lambda x: x["match_score"], reverse=True)
+    #     results.sort(key=lambda x: x["match_score"], reverse=True)
 
-        return Response(results)
+    #     return Response(results)
 
 
